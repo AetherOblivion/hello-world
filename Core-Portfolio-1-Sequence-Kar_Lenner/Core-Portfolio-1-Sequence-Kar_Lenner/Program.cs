ebb6f005bdc8fb520d9c34127354432b95ad3d0d
AetherOblivion <55197782+AetherOblivion@users.noreply.github.com>
2019-09-13 08:09:17 -0600

    Add files via upload

---
+++ Core-Portfolio-1-Sequence-Kar_Lenner/Core-Portfolio-1-Sequence-Kar_Lenner/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Portfolio_1_Sequence_Kar_Lenner
+	/* 
+	 * Purpose: To calculate the compound interest accumulated from a deposit, factoring in:
+     * the inital ammount, Interest rate, Number of years and Frequency of Compouding. 
+	 * Inputs: Initial ammount (IA), Interest rate (IR), number of years (YR) , frequency of compounding (FR)
+     * Outputs: The money in the account after the specified number of years. (Total) PW=The power in the equation.
+     * Author: Kar Lenner A06
+	 * Last Modified: 
+	 */
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            double IA, IR, YR, FR, Total;
+            
+            
+            Console.WriteLine("Please enter the ammount of your initial deposit.");
+            IA = double.Parse(Console.ReadLine());
+
+            //ask about a way to allow the user to input a % and have the program accept it//
+            //ask about that annoying thing where typing is deleting previous text and find out if youre just an idiot who cant figure it out//
+            //also delete these notes so you dont embarass yourself lmaooo//
+            //OKAY i know the issue, the use is inputting 2.5 and the program isnt converting it to the corect format? 0.025 is what it should be//
+            Console.WriteLine($"Your initial investment ammount is {IA:c} Please enter your interest rate in percent.");
+            IR = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the number of times per year (1-12) that your interest is compounded.");
+            FR = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("For how many years?");
+            YR = double.Parse(Console.ReadLine());
+
+            
+
+            Total = IA * (1+IR/FR) * Math.Pow(FR,YR);
+
+
+            Console.WriteLine($"You are earning an interest of {IR:n2}% anually, paid out {FR} time per year, for {YR} years, on your initial investment of {IA:C}.");
+            //and how to input breaks//
+            Console.WriteLine($"The ammount of money in your account after {YR} years is, {Total:C}");
+
+
+
+        }
+    }
+
+    internal class A
+    {
+    }
+}
---
+++ Core-Portfolio-1-Sequence-Kar_Lenner/Core-Portfolio-1-Sequence-Kar_Lenner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Core-Portfolio-1-Sequence-Kar_Lenner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Core-Portfolio-1-Sequence-Kar_Lenner")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99130881-fa26-497c-be8e-7d5263491435")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
